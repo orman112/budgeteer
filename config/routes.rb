8f8b723830b02a763aeb9057afd7fbb6da540698
Clayton Orman <claytonorman@gmail.com>
2015-02-20 14:23:25 -0500

    Updated signup resource and gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   #TODO Add Resources Instead of GET Request
   #get 'expense_items/index'
 
   #get 'budget_lists/index'
+
+  ####Generated from Rails Tuts
+  #get 'users/show'
+  get 'signup' => 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -12,6 +17,7 @@ Rails.application.routes.draw do
   resources :budget_lists do
     resources :expense_items
   end
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
